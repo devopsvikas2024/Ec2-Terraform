909a73a1f1c5955fd75be215c075ad7d370b804d
devopsvikas2024 <160994696+devopsvikas2024@users.noreply.github.com>
2024-02-25 20:00:26 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.region
+}
+resource "aws_security_group" {
+  name        = "SSH-security-group"
+  description = "Allow SSH traffic"
+  
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  vpc_security_group_ids = [aws_security_group.example_sg.id]
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo yum update -y
+                sudo amazon-linux-extras install docker
+                sudo service docker start
+                sudo usermod -a -G docker ec2-user
+
+   tags = {
+    Name = var.instance_name
+  }
+}
+
