d858503c5fbe269902c158ab076486aa7bf9cf87
devopsvikas2024 <160994696+devopsvikas2024@users.noreply.github.com>
2024-02-25 20:11:03 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,33 +3,34 @@ provider "aws" {
   secret_key = var.aws_secret_key
   region     = var.region
 }
-resource "aws_security_group" {
+
+resource "aws_security_group" "example_sg" {
   name        = "SSH-security-group"
   description = "Allow SSH traffic"
   
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "example_instance" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.example_sg.id]
 
   user_data = <<-EOF
-                #!/bin/bash
-                sudo yum update -y
-                sudo amazon-linux-extras install docker
-                sudo service docker start
-                sudo usermod -a -G docker ec2-user
+    #!/bin/bash
+    sudo yum update -y
+    sudo amazon-linux-extras install docker
+    sudo service docker start
+    sudo usermod -a -G docker ec2-user
+  EOF
 
-   tags = {
+  tags = {
     Name = var.instance_name
   }
 }
-
